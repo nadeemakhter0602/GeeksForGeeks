587c206fd11a7be40acaf0b29fb1b90772f3e885
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-05 19:21:11 +0530

    add wave array

---
+++ Beginner's DSA Sheet/Wave Array/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def convertToWave(self, n : int, a : List[int]) -> None:
+        # code here
+        for i in range(0, n, 2):
+            if i + 1 < n:
+                a[i], a[i + 1] = a[i + 1], a[i]
+        return a
