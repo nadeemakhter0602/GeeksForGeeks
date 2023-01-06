cae1a59b6df94c7003ab60a700ab05c2432e633a
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-06 15:39:06 +0530

    remove use of window list

--- Beginner's DSA Sheet/Maximum of all subarrays of size k/solution.py
+++ Beginner's DSA Sheet/Maximum of all subarrays of size k/solution.py
@@ -5,19 +5,16 @@ class Solution:
     #Function to find maximum of each subarray of size k.
     def max_of_subarrays(self,arr,n,k):
         #code here
-        window = deque()
         decreasing = deque()
         s = []
         for i in range(n):
             while len(decreasing) > 0 and decreasing[-1] < arr[i]:
                 decreasing.pop()
             decreasing.append(arr[i])
-            window.append(arr[i])
-            if len(window) == k:
-                if decreasing[0] == window[0]:
+            if i - k + 1 >= 0:
+                if decreasing[0] == arr[i - k + 1]:
                     max_ = decreasing.popleft()
                     s.append(max_)
                 else:
                     s.append(decreasing[0])
-                window.popleft()
         return s
